6fd0050d06e68f8da9a3bba465078b2fea7721d1
unknown <akmolina28@users.noreply.github.com>
2020-06-12 14:50:36 -0500

    Support for lovelace dashboards in HA 0.107+

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,23 @@ var gulp = require('gulp'),
   config = require('./config');
 
 gulp.task('watch', function () {
-  // Callback mode, useful if any plugin in the pipeline depends on the `end`/`flush` event
-  return watch(config.watch_path, { usePolling: true }, function (file) {
-    console.log(file.basename + " changed. Auto-touching ui-lovelace.yaml.");
-    gulp.src(config.ui_lovelace_path).pipe(touch());
-  });
+  if (!config.watch_path && !config.dashboards) {
+    console.error("You must configure either a watch_path or a dashboard in config.json. See readme for details.");
+  }
+
+  if (config.watch_path) {
+    watch(config.watch_path, { usePolling: true }, function (file) {
+      console.log(file.basename + " changed. Auto-touching ui-lovelace.yaml.");
+      gulp.src(config.ui_lovelace_path).pipe(touch());
+    });
+  }
+
+  if (config.dashboards) {
+    config.dashboards.forEach((d) => {
+      watch(d.watch_path, { usePolling: true }, function (file) {
+        console.log(file.basename + " changed. Auto-touching " + d.dashboard_path);
+        gulp.src(d.dashboard_path).pipe(touch());
+      });
+    });
+  }
 });
